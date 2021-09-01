2748cfbbd367eb3beafabb9592dea58002f34499
prem-pb <govindbiswas079@gmail.com>
2021-09-01 23:16:18 +0530

    secod commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
 import SignUP from './signup';
+import NewForm from './NewForm'
 function App() {
   const data = true;
   return (
     <div className="App">
      {/* <Login /> */}
-     <SignUP />
+     {/* <SignUP /> */}
+     <NewForm />
     </div>
   );
 }
