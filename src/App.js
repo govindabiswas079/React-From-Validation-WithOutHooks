ae3dbb1598685d0f533ba770e200211e6bb5c447
prem-pb <govindbiswas079@gmail.com>
2021-09-01 22:59:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+import SignUP from './signup';
+function App() {
+  const data = true;
+  return (
+    <div className="App">
+     {/* <Login /> */}
+     <SignUP />
+    </div>
+  );
+}
+
+export default App;
